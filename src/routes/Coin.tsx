8eb5dce4340d1f5caca1ce6b077b2bd0e5d0e18d
wooka000 <wooka000@naver.com>
2023-04-06 17:36:48 +0900

    second commit

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+
+interface RouteParams {
+  coinId: string;
+}
+
+function Coin() {
+  const { coinId } = useParams<RouteParams>();
+  return <h1>Coin</h1>;
+}
+export default Coin;
